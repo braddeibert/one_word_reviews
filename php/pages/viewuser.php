74c31680a269a516de2d0e5356b639b74f6c895f
Brad Deibert <44413459+braddeibert@users.noreply.github.com>
2020-04-09 12:27:13 -0700

    Add files via upload

---
+++ php/pages/viewuser.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+<head>
+	<title>Word</title>
+	<!-- Bootstrap 4 minified CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">	<!-- Bootstrap 4 minified JavaScript -->
+	<link rel="stylesheet" href="./style.css">
+	
+	<!-- Bootstrap 4 JS, Popper.js, jQuery -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container-fluid" id="viewport" style="padding: 0px;">
+		<div class="navbar navbar-expand-lg bg-light navbar-light">
+			<a class="navbar-brand mb-0 h1" id="brand">word.</a>
+			
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="../index.php">Home</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="./review.php">Reviews</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./movies.php">Movies</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./shows.php">Shows</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./music.php">Music</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./contributors.php">Contributors</a>
+				</li>
+				<form class="form-inline" action="./result.php" method="post">
+					<input class="form-control mr-sm-2" name="search" type="text" placeholder="Search all reviews">
+					<button class="btn btn-success" type="submit">Go</button>
+				</form>
+			</ul>
+		</div>
+
+		<div class="container">
+			<?php
+				// connect to database
+				$link=mysqli_connect("localhost", "bd152220", "ahqu3UucieGhe9vixui4chaaph8AiH", "bd152220")
+				   or die('Could not connect ');
+				   
+				$uname = $_GET['user'];
+
+				// show most recent reviews
+				$query = "SELECT username, location, bio FROM USERS WHERE username = '$uname';";
+				$result = mysqli_query($link, $query)
+						or die("Query failed - no user found");
+				
+				// print info
+				$info = mysqli_fetch_array($result, MYSQLI_ASSOC);
+				
+				echo "<h1 class='display-3'>$info[0]</h1>\n";
+				echo "<p>$info[1]</p>\n";
+				echo "<p>$info[2]</p>\n";
+
+				//Free result set
+				mysqli_free_result($result);
+
+				//close connection
+				mysqli_close($link);
+			?>
+				
+		</div>
+	</div>
+</body>
+</html>
